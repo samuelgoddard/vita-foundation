d11df9657df007fe14cee63674f5eafa33a057bd
Sam Goddard <hello@samgoddard.co.uk>
2024-06-27 21:38:26 +0100

    preview mode enabled

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '@/styles/main.css'
+
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
-import SEO from '@/helpers/seo.config';
 import { Inter } from 'next/font/google'
+
+import SEO from '@/helpers/seo.config';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -15,9 +17,26 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...SEO} /> 
+
       <div className={`${inter.variable} font-sans`}>
         <Component {...pageProps} key={router.asPath} />
       </div>
+
+      {pageProps.preview && (
+        <div
+          className={
+            "fixed bottom-0 left-0 w-auto px-2 py-2 text-white bg-[#DA442F] justify-center flex z-[1000000] uppercase text-xs m-3 font-mono tracking-wide"
+          }
+        >
+          Preview Mode -{" "}
+          <a
+            className={"px-1 underline underline-offset-2"}
+            href={`/api/exit-preview?currentRoute=${router.route}`}
+          >
+            Click To Exit
+          </a>
+        </div>
+      )}
     </>
   )
 }
