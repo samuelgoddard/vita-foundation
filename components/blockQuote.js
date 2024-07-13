93982556dc1d9afd6d596f11921736d777158c6c
Sam Goddard <hello@samgoddard.co.uk>
2024-07-13 22:15:30 +0100

    updated aspo

--- components/blockQuote.js
+++ components/blockQuote.js
@@ -1,7 +1,7 @@
 export default function Blockquote({ text, author}) {
   return (
     <blockquote className="border-l-[5px] border-orange bg-black/[0.04] p-5">
-      <p className="text-xl leading-tight md:text-2xl md:leading-tight">{text} This is a really long boy that i like it's quite nice and a very good quote a lovely one infact</p>
+      <p className="text-xl leading-tight md:text-2xl md:leading-tight">{text}</p>
       {author && (
         <cite className="not-italic mt-5 block text-sm">{author}</cite>
       )}
